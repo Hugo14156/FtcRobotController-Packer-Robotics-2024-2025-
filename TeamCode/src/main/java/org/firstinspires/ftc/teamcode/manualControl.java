d6f3ffb96b5f449ada562de3013d9ff4ffaa7a92
Hugo14156 <hugodelasnerie@gmail.com>
2024-02-21 10:44:50 -0500

    Update manualControl.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manualControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manualControl.java
@@ -64,7 +64,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear OpMode")
-@Disabled
+//@Disabled
 public class manualControl extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
