620a62aa0fc734d98941198997ba258be073e4db
Hugo14156 <hugodelasnerie@gmail.com>
2024-11-07 16:52:26 -0500

    Update MecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -64,13 +64,13 @@ public final class MecanumDrive {
 
         // drive model parameters
         public double inPerTick = 0.0224519941;
-        public double lateralInPerTick = inPerTick;
+        public double lateralInPerTick = 0.0229690522;
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
         public double kS = 0;
         public double kV = 0;
-        public double kA = 0;
+        public double kA = 0.0044;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
