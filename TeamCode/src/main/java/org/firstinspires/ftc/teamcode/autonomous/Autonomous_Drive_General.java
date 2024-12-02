e579150a5c2701afd0299ed784fb2bb81b754f49
Hugo14156 <hugodelasnerie@gmail.com>
2024-12-02 17:03:01 -0500

    Update Autonomous_Drive_General.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Autonomous_Drive_General.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Autonomous_Drive_General.java
@@ -30,6 +30,7 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Config
@@ -78,6 +79,7 @@ public class BlueSideTestAuto extends LinearOpMode {
                 }
             }
         }
+
         public Action liftUp() {
             return new LiftUp();
         }
@@ -104,7 +106,8 @@ public class BlueSideTestAuto extends LinearOpMode {
                 }
             }
         }
-        public Action liftDown(){
+
+        public Action liftDown() {
             return new LiftDown();
         }
     }
@@ -124,6 +127,7 @@ public class BlueSideTestAuto extends LinearOpMode {
         public class IntakeSample implements Action {
             private boolean initialized = false;
             private double ticks = 0.0;
+
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
@@ -132,9 +136,9 @@ public class BlueSideTestAuto extends LinearOpMode {
                     intake.setPower(1.0);
                     initialized = true;
                 }
-                if (intakePivot.getPosition() != 0.55){
+                if (intakePivot.getPosition() != 0.55) {
                     return true;
-                } else{
+                } else {
                     ticks += 1;
                 }
                 packet.put("liftPos", ticks);
@@ -147,6 +151,7 @@ public class BlueSideTestAuto extends LinearOpMode {
                 }
             }
         }
+
         public Action intakeSample() {
             return new IntakeSample();
         }
@@ -155,6 +160,7 @@ public class BlueSideTestAuto extends LinearOpMode {
             private boolean initialized = false;
             private boolean deposit = false;
             private double ticks = 0.0;
+
             @Override
             public boolean run(@NonNull TelemetryPacket packet) {
                 if (!initialized) {
@@ -163,11 +169,11 @@ public class BlueSideTestAuto extends LinearOpMode {
                     intake.setPower(0.0);
                     initialized = true;
                 }
-                if (intakePivot.getPosition() != 0.0){
+                if (intakePivot.getPosition() != 0.0) {
                     return true;
                 } else {
                     ticks += 1;
-                    if (!deposit){
+                    if (!deposit) {
                         intake.setPower(-1.0);
                         deposit = true;
                     }
@@ -182,6 +188,7 @@ public class BlueSideTestAuto extends LinearOpMode {
                 }
             }
         }
+
         public Action depositSample() {
             return new DepositSample();
         }
@@ -263,5 +270,96 @@ public class BlueSideTestAuto extends LinearOpMode {
                         trajectoryActionCloseOut
                 )
         );
+    }
+
+    private void initAprilTag() {
+
+        aprilTag = new AprilTagProcessor.Builder()
+
+                // The following default settings are available to un-comment and edit as needed.
+                //.setDrawAxes(false)
+                //.setDrawCubeProjection(false)
+                //.setDrawTagOutline(true)
+                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .setCameraPose(cameraPosition, cameraOrientation)
+
+                // == CAMERA CALIBRATION ==
+                // If you do not manually specify calibration parameters, the SDK will attempt
+                // to load a predefined calibration for your camera.
+                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
+                // ... these parameters are fx, fy, cx, cy.
+
+                .build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        //aprilTag.setDecimation(3);
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+//        builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+//        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+//        builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+//        builder.setAutoStopLiveView(false);
+
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
+
+    }   // end method initAprilTag()
+
+    private double[] localize() {
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        double[] x = new double[currentDetections.size()];
+        double[] y = new double[currentDetections.size()];
+        double[] r = new double[currentDetections.size()];
+        double[] position = new double[3];
+        int i = 0;
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                x[i] = detection.robotPose.getPosition().x;
+                y[i] = detection.robotPose.getPosition().y;
+                r[i] = detection.robotPose.getOrientation().getYaw(AngleUnit.RADIANS);
+                i += 1;
+            }   // end for() loop
+        }
+        position[0] = average(x);
+        position[1] = average(y);
+        position[2] = average(r);
+        return position;
+    }
+    public static double average(double[] array) {
+        double sum = 0;
+        for (double value : array) {
+            sum += value;
+        }
+        return sum/array.length;
     }
 }
